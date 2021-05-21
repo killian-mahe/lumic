02633962a4942107716f3e13b0e889fc1ada0e2c
Killian Mahé <killianmahe.pro@gmail.com>
2021-05-21 23:20:12 +0200

    Initial commit

---
+++ database/migrations/2021_05_20_220703_create_links_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->string('name', 20)->nullable(false);
+            $table->string('value', 500)->nullable(false);
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+
+            $table->index(['name', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function (Request $request) {
+    return Inertia::render('Dashboard', ['links' => $request->user()->links]);
+})->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/{name}', function (Request $request, string $name) {
+    $link = $request->user()->links()->where('name', $name)->first();
+    if ($link) {
+        return redirect()->away($link->value);
+    }
+    abort(404);
+});
