9456b59339e0082d6a8a1a8aab9fb25d76e2b0c3
Killian Mahé <killianmahe.pro@gmail.com>
2021-07-13 15:41:48 +0200

    Change permissions names

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -52,16 +52,16 @@ class JetstreamServiceProvider extends ServiceProvider
         Jetstream::defaultApiTokenPermissions(['read']);
 
         Jetstream::role('admin', __('Administrator'), [
-            'create',
-            'read',
-            'update',
-            'delete',
+            'links:create',
+            'links:read',
+            'links:use',
+            'links:update',
+            'links:delete',
         ])->description(__('Administrator users can perform any action.'));
 
-        Jetstream::role('editor', __('Editor'), [
-            'read',
-            'create',
-            'update',
-        ])->description(__('Editor users have the ability to read, create, and update.'));
+        Jetstream::role('user', __('User'), [
+            'links:read',
+            'links:use',
+        ])->description(__('Basic users have the ability to read and use links.'));
     }
 }
