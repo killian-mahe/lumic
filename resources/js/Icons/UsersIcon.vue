05d4904f42b20dfbfca9ad64d8f1e73aad982a83
Killian Mahé <killianmahe.pro@gmail.com>
2021-07-24 01:33:40 +0200

    Update link form with visibility

---
+++ resources/js/Icons/UsersIcon.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" :class="computedClass" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+</template>
+
+<script>
+export default {
+    name: "UsersIcon.vue",
+    props: {
+        size: {
+            default: "24"
+        }
+    },
+    computed: {
+        computedClass() {
+            return `feather feather-users w-${this.size} h-${this.size}`
+        }
+    }
+}
+</script>
