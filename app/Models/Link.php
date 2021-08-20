3f40d32514733e6da61c238d5f2a4e8087b672cf
Killian Mahé <killianmahe.pro@gmail.com>
2021-08-20 11:15:51 +0200

    :sparkles: Add a favicon in links

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Http\UploadedFile;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Link extends Model
 {
@@ -26,6 +28,15 @@ class Link extends Model
      * @var array
      */
     protected $hidden = [
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'favicon_url',
     ];
 
     /**
@@ -56,5 +67,62 @@ class Link extends Model
     public function isPublic(): bool
     {
         return $this->public;
+    }
+
+    /**
+     * Update the link's favicon.
+     *
+     * @param  \Illuminate\Http\UploadedFile  $favicon
+     * @return void
+     */
+    public function updateFavicon(UploadedFile $favicon)
+    {
+        tap($this->favicon, function ($previous) use ($favicon) {
+            $this->forceFill([
+                'favicon_path' => $favicon->storePublicly(
+                    'links-favicons', ['disk' => $this->faviconDisk()]
+                ),
+            ])->save();
+
+            if ($previous) {
+                Storage::disk($this->faviconDisk())->delete($previous);
+            }
+        });
+    }
+
+    /**
+     * Get the URL to the user's profile photo.
+     *
+     * @return string
+     */
+    public function getFaviconUrlAttribute()
+    {
+        return $this->favicon_path
+            ? Storage::disk($this->faviconDisk())->url($this->favicon_path)
+            : null;
+    }
+
+    /**
+     * Get the disk that favicon should be stored on.
+     *
+     * @return string
+     */
+    protected function faviconDisk()
+    {
+        return isset($_ENV['VAPOR_ARTIFACT_NAME']) ? 's3' : config('jetstream.profile_photo_disk', 'public');
+    }
+
+    /**
+     * Delete the user's profile photo.
+     *
+     * @return void
+     */
+    public function deleteFavicon()
+    {
+        Storage::disk($this->faviconDisk())->delete($this->favicon_path);
+
+        $this->forceFill([
+            'favicon_path' => null,
+        ])->save();
     }
 }
