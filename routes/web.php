9f5163db02fb02811ab53519200b5cf71cb30db0
Killian Mahé <killianmahe.pro@gmail.com>
2021-08-21 20:13:14 +0200

    :truck: Redirect main page to dashboard when connected

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect('dashboard');
 });
 
 Route::middleware('auth:sanctum')->group(function() {
