81f0ab054961f5749f682b1b8b37c3402d1c221c
Killian Mahé <killianmahe.pro@gmail.com>
2021-08-22 15:40:28 +0200

    :construction: Test

--- resources/js/Pages/Administration/Show.vue
+++ resources/js/Pages/Administration/Show.vue
@@ -5,15 +5,25 @@
                 Administration Panel
             </h2>
         </template>
+
+        <div>
+            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+
+                <website-management/>
+
+            </div>
+        </div>
     </app-layout>
 </template>
 
 <script>
 import AppLayout from '@/Layouts/AppLayout'
+import WebsiteManagement from "./WebsiteManagement";
 
 export default {
     components: {
-        AppLayout
+        AppLayout,
+        WebsiteManagement
     }
 }
 </script>
