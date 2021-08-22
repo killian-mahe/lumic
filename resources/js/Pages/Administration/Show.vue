867f1dd41651f96d4a690f64f4b3d9b8777d1dd7
Killian Mahé <killianmahe.pro@gmail.com>
2021-08-22 12:03:08 +0200

    :building_construction: Create administration panel component

---
+++ resources/js/Pages/Administration/Show.vue
@@ -0,0 +1,23 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Administration Panel
+            </h2>
+        </template>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout'
+
+export default {
+    components: {
+        AppLayout
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
