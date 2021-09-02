73b597d1333d82550302df9c84ed7584fa9b7c07
Killian Mahé <killianmahe.pro@gmail.com>
2021-09-02 11:32:42 +0200

    🔧 Create configuration table

---
+++ database/migrations/2021_09_02_085638_create_config_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConfigTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('config', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('key')->nullable(false)->unique();
+            $table->json('value')->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('config');
+    }
+}
