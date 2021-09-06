8a85fe64bf192ecc94acf6d83678d9e20977d49a
Killian Mahé <killianmahe.pro@gmail.com>
2021-09-06 15:18:38 +0200

    :bug: Fix middleware authentication check

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            "is_website_admin" => $request->user()->isWebsiteAdmin()
+            "is_website_admin" => $request->user()?->isWebsiteAdmin()
         ]);
     }
 }
