b8a5d6995ca08ca65848d6cd4aeb97c48ed84f1b
Killian Mahé <killianmahe.pro@gmail.com>
2021-09-16 23:45:41 -0400

    🚚 Create Analytics dashboard view

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -20,6 +20,10 @@
                                 <jet-nav-link :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </jet-nav-link>
+
+                                <jet-nav-link :href="route('analytics')" :active="route().current('analytics')">
+                                    Analytics
+                                </jet-nav-link>
                             </div>
                         </div>
 
--- resources/js/Pages/Analytics/Show.vue
+++ resources/js/Pages/Analytics/Show.vue
@@ -2,28 +2,23 @@
     <app-layout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Administration Panel
+                Analytics - {{ $page.props.user.current_team.name }}
             </h2>
         </template>
 
-        <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
 
-                <website-management/>
-
-            </div>
-        </div>
     </app-layout>
 </template>
 
 <script>
 import AppLayout from '@/Layouts/AppLayout';
-import WebsiteManagement from "./WebsiteManagement";
 
 export default {
     components: {
         AppLayout,
-        WebsiteManagement
+    },
+    mounted() {
+        console.log(this.$page.props.statistics)
     }
 }
 </script>
