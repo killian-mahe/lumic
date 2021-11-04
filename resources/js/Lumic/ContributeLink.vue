83d6a32c6a9525fb7126d8781d15266401aaa6ca
Killian Mahé <killianmahe.pro@gmail.com>
2021-11-03 23:10:40 -0400

    ✨ Create a GitHub contribute link

---
+++ resources/js/Lumic/ContributeLink.vue
@@ -0,0 +1,16 @@
+<template>
+    <a href="https://github.com/killian-mahe/lumic/issues">
+        <img class="transition-all duration-300 ease-in-out p-2 bg-white rounded-full shadow-md hover:opacity-100 opacity-50 w-16 h-auto hover:shadow-lg" src="/img/github/GitHub-Mark-64px.png" />
+    </a>
+</template>
+
+<script>
+export default {
+    name: "HelpUs"
+
+}
+</script>
+
+<style scoped>
+
+</style>
