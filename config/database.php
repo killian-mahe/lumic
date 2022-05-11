5e9296a98d23e413c983ea67a20ce1e3025e5761
Killian Mahé <killianmahe.pro@gmail.com>
2022-05-10 23:51:02 -0400

    Update pem certificate

--- config/database.php
+++ config/database.php
@@ -58,9 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => (env('MYSQL_SSL') && extension_loaded('pdo_mysql')) ? [
+                PDO::MYSQL_ATTR_SSL_KEY    => '/ssl/DigiCertGlobalRootCA.crt.pem',
+            ] : [],
         ],
 
         'pgsql' => [
